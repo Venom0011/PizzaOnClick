da1fe9090d612c5fe9e5a464c0bb649a06c63178
Venom0011 <abhishekmahajan255@gmail.com>
2023-12-07 22:46:48 +0530

    first commit

---
+++ front-end/src/Components/AllPizza.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllPizza() {
+  return (
+    <div>AllPizza</div>
+  )
+}
+
+export default AllPizza
---
+++ front-end/src/Components/PizzaDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PizzaDetails() {
+  return (
+    <div>PizzaDetails</div>
+  )
+}
+
+export default PizzaDetails
